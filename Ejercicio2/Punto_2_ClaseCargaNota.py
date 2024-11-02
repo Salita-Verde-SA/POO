d2f89e7993d89228073a5249bb895d36fbc0ac98
Fabrizio Castillo <138902138+FabrizioCastillo@users.noreply.github.com>
2024-11-01 21:07:40 -0300

    Add files via upload

---
+++ Ejercicio2/Punto_2_ClaseCargaNota.py
@@ -0,0 +1,54 @@
+from Punto_2_ClaseAlumno import Alumno
+from Punto_2_ClaseNota import Nota
+
+
+class CargaNotas:
+    def __init__(self):
+        self.alumnos = []
+
+    def main(self):
+        self.cargar_alumnos()
+        self.mostrar_informacion()
+
+    def cargar_alumnos(self):
+        while True:
+
+            nombre = input("Ingrese el nombre completo del alumno: ")
+            legajo = int(input("Ingrese el legajo: "))
+            alumno = Alumno(nombre, legajo)
+            self.cargar_notas(alumno)
+            self.alumnos.append(alumno)
+            respuesta = input("Deseas cargar mas alumnos? (1.SI / 2.NO): ")
+            if respuesta == "2":
+                break
+
+    def cargar_notas(self, alumno):
+        while True:
+
+            catedra = input("Ingrese la catedra: ")
+            notaExamen = float(
+                input(f"Ingrese la nota del alumno {alumno.nombreCompleto}: ")
+            )
+            nota = Nota(catedra, notaExamen)
+            alumno.cargar_notas(nota)
+            respuesta = input("Deseas cargar mas notas? (1.SI / 2.NO): ")
+            if respuesta == "2":
+                break
+
+    def mostrar_informacion(self):
+
+        for alumno in self.alumnos:
+            print()
+            print(f"Alumno: {alumno.nombreCompleto} / Legajo: {alumno.legajo}")
+            for nota in alumno.notas:
+                print(f"_Catedra: {nota.catedra} / Nota: {nota.notaExamen}")
+                print()
+
+            promedio = alumno.obtener_promedio()
+            print(f"Promedio: {promedio:.2f}")
+            print("---------------------------------")
+
+
+if __name__ == "__main__":
+    carga_notas = CargaNotas()
+    carga_notas.main()
---
+++ Ejercicio3/Punto_3_ClaseMenuRestaurant.py
@@ -0,0 +1,81 @@
+from Punto_3_ClaseIngrediente import Ingredientes
+from Punto_3_ClasePlato import Plato
+
+
+class MenuRestaurant:
+    def __init__(self):
+        self.platosMenu = []
+
+    def main(self):
+        self.cargar_platos()
+        self.mostrar_informacion()
+
+    def cargar_platos(self):
+
+        while True:
+            nombre = input("Ingrese el nombre del plato: ")
+            precio = float(input("Ingrese el precio: "))
+            esBebida = input("Es una bebida?: (1.SI / 2.NO)")
+            while True:
+                if esBebida == "1":
+                    esBebida = True
+                    break
+                elif esBebida == "2":
+                    esBebida = False
+                    break
+                else:
+                    print("Ingrese una respuesta valida.")
+            plato = Plato(nombre, precio, esBebida)
+            if esBebida == False:
+                self.cargar_ingredientes(plato)
+
+            self.platosMenu.append(plato)
+            respuesta = input("Quiere ingresar otro plato? (1.SI / 2.NO): ")
+            if respuesta == "2":
+                break
+
+    def cargar_ingredientes(self, plato):
+        # if nIngredientes <= 0:
+        #   print("Ingrese por lo menos un ingrediente.")
+        #  self.cargar_ingredientes(plato)
+        # return
+        print(f"Plato: {plato.nombreCompleto}")
+        if plato.esBebida == True:
+            print("El plato es una bebida y no lleva ingredientes.")
+        elif plato.esBebida == False:
+            while True:
+                nombre = input("Ingrese el nombre del ingrediente: ")
+                if nombre == "":
+                    print("Ingrese por lo menos un ingrediente.")
+                    self.cargar_ingredientes(plato)
+                    return
+                cantidad = float(input("Ingrese la cantidad: "))
+                unidadDeMedida = input("Ingrese la unidad de medida: ")
+                ingrediente = Ingredientes(nombre, cantidad, unidadDeMedida)
+                plato.cargar_lista_ingredientes(ingrediente)
+                respuesta = input("Quiere ingresar otro ingrediente? (1.SI / 2.NO): ")
+                if respuesta == "2":
+                    break
+
+    def mostrar_informacion(self):
+        print("-----------MENÚ-----------")
+        for plato in self.platosMenu:
+            print(f"Nombre: {plato.nombreCompleto}\nPrecio: ${plato.precio}")
+            if plato.esBebida == True:
+                print()
+                print("-----------------------------------------")
+            elif plato.esBebida == False:
+                print("Ingredientes:")
+                print(f"{'UNIDAD DE MEDIDA':<15}   {'CANTIDAD':<15}   NOMBRE ")
+                for ingrediente in plato.listaIngredientes:
+
+                    print(
+                        f"{ingrediente.unidadDeMedida:<15}    {ingrediente.cantidad:<15}   {ingrediente.nombre}"
+                    )
+                print()
+                print("-----------------------------------------")
+
+
+if __name__ == "__main__":
+    menu = MenuRestaurant()
+    menu.main()
